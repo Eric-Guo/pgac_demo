b67af62d8cf2f5b7b2a733518233d00215892349
Eric Guo <eric.guo@sandisk.com>
2016-01-26 22:14:26 +0800

    rails g channel room speak

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak
+  end
+end
