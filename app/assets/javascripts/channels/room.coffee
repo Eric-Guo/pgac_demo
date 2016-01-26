f684bb72363c0c0b89e07540014bd12cb3d2942c
Eric Guo <eric.guo@sandisk.com>
2016-01-26 22:24:42 +0800

    App.room.speak('hello world') and received from broadcast. (restart server, no auto re-load yet for ActionCable)

--- app/assets/javascripts/channels/room.coffee
+++ app/assets/javascripts/channels/room.coffee
@@ -6,7 +6,7 @@ App.room = App.cable.subscriptions.create "RoomChannel",
     # Called when the subscription has been terminated by the server
 
   received: (data) ->
-    # Called when there's incoming data on the websocket for this channel
+    alert data['message']
 
-  speak: ->
-    @perform 'speak'
+  speak: (message) ->
+    @perform 'speak', message: message
--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,13 +1,14 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from 'room_channel'
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    ActionCable.server.broadcast 'room_channel', message: data['message']
   end
 end
