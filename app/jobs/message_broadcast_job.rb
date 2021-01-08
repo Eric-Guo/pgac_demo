f4dfaf62ec02ef46b9a31f92917129b6da4346dd
Eric-Guo <eric.guocz@gmail.com>
2021-01-08 16:59:42 +0800

    One place need to fix.

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,7 +2,7 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast 'room_channel', message: render_message(message)
+    ActionCable.server.broadcast 'room_channel', { message: render_message(message) }
   end
 
   private
