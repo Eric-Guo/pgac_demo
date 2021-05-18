5f73220ba72cc21822b4cc35b5c233a48c1f025f
Eric-Guo <eric.guocz@gmail.com>
2021-05-18 09:37:20 +0800

    Upgrade to Rails 6.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'rooms#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
