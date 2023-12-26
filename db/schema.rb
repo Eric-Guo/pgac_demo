3882ffd6cc89f5b6db51c4bd8fa6479383ce80de
Eric Guo <eric.guocz@gmail.com>
2023-12-26 11:07:22 +0800

    Running 7.1 default.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2021_01_08_084657) do
-
+ActiveRecord::Schema[7.1].define(version: 2021_01_08_084657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", id: :serial, force: :cascade do |t|
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
 end
