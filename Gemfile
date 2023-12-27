5bb08a57674af2d9bdf276512801b08fbcfe4c31
Eric Guo <eric.guocz@gmail.com>
2023-12-27 16:13:04 +0800

    Using Sqlite3 as any cable using redis.

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '>= 3.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 7.1.2'
 
-gem 'pg'
+gem 'sqlite3'
 
 gem 'anycable-rails'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2021_01_08_084657) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "messages", id: :serial, force: :cascade do |t|
+  create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
