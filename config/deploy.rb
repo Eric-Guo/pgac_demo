b416c8e8cc33e71e34ae76bf42df99d16cfaccc8
Eric Guo <eric.guocz@gmail.com>
2024-06-27 12:45:22 +0800

    Bump capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.18.1"
+lock "~> 3.19.0"
 
 set :application, "pgac"
 set :repo_url, "https://github.com/Eric-Guo/pgac_demo.git"
