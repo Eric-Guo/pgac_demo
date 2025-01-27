e38dec3d8ad34c68cddd33d7c40e98dbe5166830
Eric Guo <eric.guocz@gmail.com>
2025-01-27 21:35:52 +0800

    Go ruby 3.3.7

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ append :linked_dirs, *%w[log tmp/pids tmp/cache tmp/sockets public/assets storag
 # set :ssh_options, verify_host_key: :secure
 
 set :rbenv_type, :user
-set :rbenv_ruby, "3.2.6"
+set :rbenv_ruby, "3.3.7"
 
 set :puma_init_active_record, true
 set :puma_phased_restart, true
