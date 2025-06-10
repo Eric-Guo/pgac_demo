63f57abe34be053a4d0346d3fc03a3398fad1e23
Eric-Guo <eric.guocz@gmail.com>
2025-06-10 17:11:31 +0800

    ✨I need store fingerprint to cookie, so when page refresh, the show action in RoomsController can set the current user, similar in speak method in RoomChannel

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,7 +1,30 @@
+// Helper function to set cookies
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Helper function to get cookies
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
 App.room = App.cable.subscriptions.create("RoomChannel", {
   connected: function() {
     var fingerprint = new Fingerprint().get();
-    localStorage.setItem('fingerprint', fingerprint);
+    setCookie('fingerprint', fingerprint, 365); // Store for 1 year
   },
 
   disconnected: function() {
@@ -13,7 +36,7 @@ App.room = App.cable.subscriptions.create("RoomChannel", {
   },
 
   speak: function(message) {
-    var fingerprint = localStorage.getItem('fingerprint');
+    var fingerprint = getCookie('fingerprint');
     this.perform('speak', { message: message, fingerprint: fingerprint });
   }
 });
